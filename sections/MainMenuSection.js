2b7741117ca9b8e16eaa6839835579ca02553a25
Katherinegf <110284567+Katherinegf@users.noreply.github.com>
2022-10-26 08:29:10 +0200

    Add files via upload

---
+++ sections/MainMenuSection.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import MenuIcon from '../components/MenuIcon';
+
+const MainMenuSection = () => {
+    return (
+    
+        <nav className ="mainmenu container">
+           
+        <div className="logo">
+            Fixxo.
+        </div>
+        <div className="menu-links">
+            <NavLink className="menu-link" to="/">Home</NavLink>
+            <NavLink className="menu-link" to="/categories">Categories</NavLink>
+            <NavLink className="menu-link" to="/products">Products</NavLink>
+            <NavLink className="menu-link" to="/contacts">Contacts</NavLink>
+        </div>
+        
+        <div className="menu-icons"> 
+            <MenuIcon link="/search" icon="fa-regular fa-magnifying-glass" />
+            <MenuIcon link="/compare" icon="fa-regular fa-repeat" />
+            <MenuIcon link="/wishlist" icon="fa-regular fa-heart" />
+            <MenuIcon link="/shoppingcart" icon="fa-regular fa-bag-shopping" />
+        </div>
+    </nav>
+           
+
+    )
+}
+
+export default MainMenuSection
